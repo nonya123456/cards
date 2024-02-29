5bb870bc71986b8acc8ca606d650e61b9f2e3349
nonya123456 <nonya579@gmail.com>
2024-02-29 23:17:11 +0700

    add text when null

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,25 +9,27 @@ const Card: FC<CardProps> = ({ index, handleDraw }) => {
     const suits = ["♧", "♢", "♡", "♤"]
     const ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
-    const getSuit = (index: number | null) => {
-        if (index === null) {
-            return null
-        }
+    const getSuit = (index: number) => {
         return suits[index % 4]
     }
 
-    const getRank = (index: number | null) => {
-        if (index === null) {
-            return null
-        }
+    const getRank = (index: number) => {
         return ranks[Math.floor(index / 4)]
     }
 
     return (
         <div className="card" onClick={() => handleDraw()}>
-            <p>{getRank(index)}</p>
-            <p>{getSuit(index)}</p>
-        </div>
+            {
+                index === null ? (
+                    <p>Tap to Draw</p>
+                ) : (
+                    <>
+                        <p>{getRank(index)}</p>
+                        <p>{getSuit(index)}</p>
+                    </>
+                )
+            }
+        </div >
     );
 };
 
